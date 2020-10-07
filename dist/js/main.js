3b375b6a8bad4258040415757730686429a34cde
Nick <deckards2505@gmail.com>
2020-10-07 15:32:38 +0600

    feat: add first-screen (need add slider and adaptive)

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,4 +3,26 @@ $(function() {
         $('.navigation__list').slideToggle();
         $(this).toggleClass('active');
     });
+
+    
+
+    // Main screen checkboxes /
+
+    $('.main-slider__form-area-btn').on('click', function() {
+        $('.main-slider__form-area').toggleClass('active');
+    });
+
+    $('.main-slider__form-price-btn').on('click', function() {
+        $('.main-slider__form-price').toggleClass('active');
+    });
+
+    $(document).on('click', function(e){
+        if (!(e.target.closest('.main-slider__form-area'))) {
+            $('.main-slider__form-area').removeClass('active');
+        }
+        if (!(e.target.closest('.main-slider__form-price'))) {
+            $('.main-slider__form-price').removeClass('active');
+        }
+    });
+
 });
